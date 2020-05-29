7cc6e6d150fade38296abb6fe9ca9ae594f3fd98
Matt Bierner <mattbierner@gmail.com>
2020-05-28 18:43:10 -0700

    Update webpack versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 "use strict";
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    entry: {
-        main: './src/main.js'
-    },
-    output: {
-        path: path.join(__dirname, "js"),
-        filename: "[name].js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
+module.exports = (env, argv) => {
+    const production = env && env.production;
+    return {
+        mode: production ? 'production' : 'development',
+        entry: {
+            main: './src/main.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
                 }
-            }
-        ]
-    }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+        output: {
+            filename: "[name].js",
+            path: path.join(__dirname, "js")
+        },
+        devtool: production ? '' : 'inline-source-map',
+        optimization: {
+            minimize: !!production,
+            minimizer: [new TerserPlugin()],
+        },
+    };
 };
